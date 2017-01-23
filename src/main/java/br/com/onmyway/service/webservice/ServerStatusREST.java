9370915e483f5571ec77a9613b621cd4ab83289d
Fellipe Sombra <fellipe__sombra@hotmail.com>
2017-01-23 07:02:45 -0200

    Adicionado serviço rest para verificar se o servidor está online antes de entrar no app

---
+++ src/main/java/br/com/onmyway/service/webservice/ServerStatusREST.java
@@ -0,0 +1,22 @@
+package br.com.onmyway.service.webservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import br.com.onmyway.valueobject.ContatoDTO;
+
+@Path("/server")
+public class ServerStatusREST {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response check(){
+	Response response = Response.status(Status.OK).entity(new ContatoDTO()).build();
+	return response;
+    }
+
+}
